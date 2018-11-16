a5b1e6e34197ddf63ad9c6dc18a2050bb3262b81
Samuel <djorester@gmail.com>
2018-11-16 16:27:56 -0700

    Fechas actuales automaticas

--- polls/static/polls/JS/EntradasSalidas.js
+++ polls/static/polls/JS/EntradasSalidas.js
@@ -1,5 +1,26 @@
+$(document).ready(function() {
+    var hoy = new Date();
+    var dd = hoy.getDate();
+    var mm = hoy.getMonth() + 1;
+    var yyyy = hoy.getFullYear();
+    $("#Fecha").val(yyyy+"-"+mm+"-"+dd);
+});
 $("#CodigoP").change(function(){
 var option = $('option:selected', this).attr('identificador');
 $("#Descripcion").val(Codigo[option]);
 $("#Controlador").val(option);
+});
+$('#Nentrada').keydown(function(e) {
+var key = e.which;
+if (key==112){
+  e.preventDefault();
+  console.log("Nos brincamos el f1");
+}
+});
+$('#Nsalida').keydown(function(e) {
+var key = e.which;
+if (key==112){
+  e.preventDefault();
+  console.log("Nos brincamos el f1");
+}
 });
